5f2465d3da9c9abef669a0bf657f2040f0a8571e
Sarmat Khachirov <58251349+sarmatdev@users.noreply.github.com>
2021-11-09 11:44:27 +0300

    Add files via upload

---
+++ build/precache-manifest.b876af8335b1bf2a6337ca8b82564be9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c66a2f20fe2afe8c60579c8b1dddebf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9437e35b223eb3dfc611",
+    "url": "/static/css/main.06b04e02.chunk.css"
+  },
+  {
+    "revision": "cdfb76ef041dc11b2774",
+    "url": "/static/js/2.a32c65bf.chunk.js"
+  },
+  {
+    "revision": "20a6f33d71d567d1fe0dc1c77a1a22e7",
+    "url": "/static/js/2.a32c65bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9437e35b223eb3dfc611",
+    "url": "/static/js/main.7cf33787.chunk.js"
+  },
+  {
+    "revision": "e895942ef6c3f9ecc233",
+    "url": "/static/js/runtime-main.7c8f0a30.js"
+  }
+]);
